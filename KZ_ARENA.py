889af7fa357e827970f404354a1e4b7ed691c83c
stanislaumich <staslz42stas@gmail.com>
2023-03-31 00:30:09 +0300

    AUTO FROM HOME 31.03.2023  0:30:09,70

---
+++ KZ_ARENA.py
@@ -0,0 +1,51 @@
+'''
+разобрать и посчитать такой формат из экселя
+walmau 6altau;107;DominosZ;151;walmau 6altau;99;Макыс;77;Макыс;124;**SUPERHelen**;130;**SUPERHelen**;144;ЛюЛюНдРа=;140;DambLDoR;97;**SUPERHelen**;151;
+бешка-бебешка_;105;Бродвей;139;T@nk;97;T@nk;63;T@nk;102;бешка-бебешка_;98;StasLz42;125;**SUPERHelen**;131;**SUPERHelen**;63;DambLDoR;129;
+*Милка;100;Kerberos;136;Тудор;93;DambLDoR;61;Тудор;100;ЛюЛюНдРа=;95;Kerberos;116;Kerberos;111;Kerberos;53;Kerberos;78;
+
+'''
+base = "arena_mart"
+def main():
+    d = {}
+    with open(base + ".csv", "r") as file:
+        content = file.read()
+        content = content.replace("\n", ";") #
+        #
+        for i in range(1, 10):
+            content = content.replace(";;", ";")
+        #
+        ar = content.split(";")
+        s = 0
+        rs = ""
+        lst = []
+        for r in ar:
+            #print(r)
+            rs = rs + r + ";"
+            s = s + 1
+            if s == 2:
+                s = 0
+                t = rs.split(";")
+                rs = ""
+                if t[0] != "":
+                    bob = (t[0], t[1])
+                    lst.append(bob)
+
+        for r in lst:
+            print(r)
+            #print(d.setdefault(r[0], 0))
+            d[r[0]] = d.setdefault(r[0], 0) + int(r[1])
+            print(d[r[0]])
+
+    with open(base+"RES.csv", "w") as file:
+        for key, value in d.items():
+            s = (key+";"+str(value) + "\n")
+            file.write(s)
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
