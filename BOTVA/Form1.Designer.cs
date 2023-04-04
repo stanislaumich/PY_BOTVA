95c860f7e1d1b5e38379d1bce1ef1932ad9fe228
stanislaumich <staslz42stas@gmail.com>
2023-04-04 23:38:37 +0300

    AUTO FROM HOME 04.04.2023 23:38:36,88

--- BOTVA/Form1.Designer.cs
+++ BOTVA/Form1.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
             SuspendLayout();
             // 
             // button1
@@ -37,23 +40,59 @@
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 0;
-            button1.Text = "button1";
+            button1.Text = "Запустить";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(549, 12);
+            button2.Name = "button2";
+            button2.Size = new Size(77, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Отменить";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(13, 15);
+            label1.TabIndex = 2;
+            label1.Text = "0";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(31, 6);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(261, 265);
+            textBox1.TabIndex = 3;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button2);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
+            FormClosing += Form1_FormClosing;
+            Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
+        private Button button2;
+        private Label label1;
+        private TextBox textBox1;
     }
 }
--- BOTVA/Form1.cs
+++ BOTVA/Form1.cs
@@ -1,9 +1,18 @@
+using OpenQA.Selenium;
+using Selenium.Extensions;
 using Selenium.WebDriver.UndetectedChromeDriver;
+using System.Xml.Linq;
+
+
 
 namespace BOTVA
 {
+    
+
     public partial class Form1 : Form
+
     {
+        public UndetectedChromeDriver? driver;
         public Form1()
         {
             InitializeComponent();
@@ -11,13 +20,81 @@ namespace BOTVA
 
         private void button1_Click(object sender, EventArgs e)
         {
-            using (var driver = UndetectedChromeDriver.Instance("BOTVA"))
+            driver = (UndetectedChromeDriver?)UndetectedChromeDriver.Instance("BOTVA");
+            label1.Text = "0";
+
+            try
             {
-                driver.GoTo("https://google.com");
-                try
-                driver.FindElement("css selector", "uy");
-                
+                driver.GoTo("https://g1.botva.ru");
+                driver.FindElement(By.ClassName("sign_in")).Click();
+                driver.FindElement(By.Name("email")).Clear();
+                driver.FindElement(By.Name("email")).SendKeys("123");
+                driver.FindElement(By.Name("password")).Clear();
+                driver.FindElement(By.Name("password")).SendKeys("123");
             }
+            catch
+            {
+                button1.Text = "NO LOGIN";
+                textBox1.Text = textBox1.Text + "NO LOGIN \n";
+            }
+            finally
+            {
+
+            }
+            //////////////////////////////////////////////////////
+            driver.GoTo("https://g1.botva.ru/monster.php?a=monsterpve");
+            do
+            {
+                try
+                {
+
+                    driver.FindElement(By.XPath("/html/body/div[5]/div[3]/div[2]/div[2]/div[2]/div[4]/form/input[3]")).Click();
+                    button1.Text = "good";
+                    textBox1.Text = textBox1.Text + "good \n";
+                    label1.Text = "1";
+                }
+                catch
+                {
+                    button1.Text = "NOT FOUND";
+                    textBox1.Text = textBox1.Text + "NOT FOUND \n";
+                    for (int i = 0; i < 100; i++)
+                    {
+                        System.Threading.Thread.Sleep(50);
+                        Application.DoEvents();
+                    }
+                    label1.Text = "0";
+                }
+                finally
+                {
+                    //System.Threading.Thread.Sleep(5000);
+                }
+                driver.Refresh();
+            }
+            while (label1.Text == "0");
+            ////driver.stop
+
+            driver.Close();
+            driver.Quit();
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "1";
+            driver.Close();
+            driver.Quit();
         }
     }
 }
